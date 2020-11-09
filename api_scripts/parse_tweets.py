9d52eff2765f83d7bbb83826467586b884741b45
Baker <cbaker4@ashtonlab3md-13.home>
2020-11-09 15:08:04 -0500

    Wrapping the bio location as it is a user defined field

--- api_scripts/parse_tweets.py
+++ api_scripts/parse_tweets.py
@@ -25,10 +25,10 @@ def print_tweet(json_list):
 	tweet_geo = data_list['geo']
 	tweet_coord = data_list['coordinates']
 	tweet_lang = data_list['lang']
-	print('%s,%s,"%s","%s","%s",%s,%s,%s,%s,%s,%s,%s,%s,%s,%s'%(str(tweet_id),str(tweet_created_at),str(tweet_text),','.join(tweet_hashtags),','.join(tweet_urls),str(user_id),str(user_sn),str(user_location),str(user_followers),str(user_friends),str(user_created),str(user_verified),str(tweet_geo),str(tweet_coord),tweet_lang))
+	print('%s,%s,"%s","%s","%s",%s,%s,"%s",%s,%s,%s,%s,%s,%s,%s'%(str(tweet_id),str(tweet_created_at),str(tweet_text),','.join(tweet_hashtags),','.join(tweet_urls),str(user_id),str(user_sn),str(user_location),str(user_followers),str(user_friends),str(user_created),str(user_verified),str(tweet_geo),str(tweet_coord),tweet_lang))
 
 with open(sys.argv[1]) as json_file:
-	print("tweet_id,tweet_created_at,tweet_test,tweet_hashtages,tweet_urls,user_id,user_name,user_location,num_followers,num_following,user_creation_date,user_verified,tweet_location,tweet_coordinates")
+	print("tweet_id,tweet_created_at,tweet_text,tweet_hashtags,tweet_urls,user_id,user_name,user_location,num_followers,num_following,user_creation_date,user_verified,tweet_location,tweet_coordinates,language")
 	for line in json_file.readlines():
 		if "errors" not in line and len(line.strip()) > 0:
 			#print(line)
